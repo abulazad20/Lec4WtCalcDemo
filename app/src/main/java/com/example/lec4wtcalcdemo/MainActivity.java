0e76a1578a6ea072160dff81a9b2dba877a04636
abulazad20 <chandbatchaa@student.douglascollege.ca>
2023-02-02 11:25:13 -0800

    Second commit from locval machin

--- app/src/main/java/com/example/lec4wtcalcdemo/MainActivity.java
+++ app/src/main/java/com/example/lec4wtcalcdemo/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
+// made change locally in lab computer
 public class MainActivity extends AppCompatActivity {
 
     RadioGroup redGroupConvType;
